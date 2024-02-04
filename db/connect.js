3d8fb8cc948b52af9bedbd7521bb979dd83a326e
Gupta <rajat.aj.gupta@accenture.com>
2024-02-04 16:58:53 +0530

    changes to fix mongoose

--- db/connect.js
+++ db/connect.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 
-// uri = "mongodb+srv://rajat:Rajat@3944@cluster0.pv92mgy.mongodb.net/Cluster0?retryWrites=true&w=majority"
-// uri = "mongodb+srv://rajat:Rajat@3944@cluster0.pv92mgy.mongodb.net/?retryWrites=true&w=majority"
-// const uri = "mongodb+srv://rajat:Rajat%403944@cluster0.pv92mgy.mongodb.net/Cluster0?retryWrites=true&w=majority";
-
 
 const connectDB = (uri) => {
-    console.log("connect db")
     return mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+    })
+    // .then(() => {
+    //     console.info("Database connected");
+    //   })
+    //   .catch((error) => {
+    //     console.error("db error", error);
+    //     process.exit(1);
+    //   });
 }
 
 module.exports = connectDB;
