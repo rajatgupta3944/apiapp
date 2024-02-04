b6cc693aa0e8d54d38919205fc4a0ebfff87078e
Gupta <rajat.aj.gupta@accenture.com>
2024-02-04 17:32:27 +0530

    fixing server undefined issue

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 
 const connectDB = (uri) => {
-    return mongoose.connect(uri)
+    return mongoose.connect(uri, {
+        useNewUrlParser: "true",
+        useUnifiedTopology: "true"
+      })
 }
 
 module.exports = connectDB;
