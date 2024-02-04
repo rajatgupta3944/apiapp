e28e47d423c1e13748aa40d5c3c4feaed1461607
Gupta <rajat.aj.gupta@accenture.com>
2024-02-04 17:08:17 +0530

    changes in connect removing empty object

--- db/connect.js
+++ db/connect.js
@@ -2,17 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDB = (uri) => {
-    return mongoose.connect(uri, {
-        // useNewUrlParser: true,
-        // useUnifiedTopology: true,
-    })
-    // .then(() => {
-    //     console.info("Database connected");
-    //   })
-    //   .catch((error) => {
-    //     console.error("db error", error);
-    //     process.exit(1);
-    //   });
+    return mongoose.connect(uri)
 }
 
 module.exports = connectDB;
